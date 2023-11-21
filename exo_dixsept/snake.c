78f6e7a13bf2eb61fff86afefd1d304cf5e8d8d2
Kenneth SOARES <kensoares@gmail.com>
2023-11-21 10:01:11 +0100

    bug fixes left

--- exo_dixsept/snake.c
+++ exo_dixsept/snake.c
@@ -13,6 +13,7 @@ typedef struct {
 typedef struct {
     int x;
     int y;
+    int hit;
 } Fruit;
 
 typedef struct {
@@ -24,8 +25,44 @@ typedef struct {
 
 /* DECLARATION DE FONCTION */
 
+void verif_depl(char depl, Serpent *s){
+    switch(depl){
+        case 'w':
+            s->dy = -1;
+            s->dx = 0;
+            break;
+        case 's':
+            s->dy = 1;
+            s->dx = 0;
+            break;
+        case 'a':
+            s->dx = -1;
+            s->dy = 0;
+            break;
+        case 'd':
+            s->dx = 1;
+            s->dy = 0;
+            break;
+        default:
+            break;
+    }
+}
+
+void depl_s(char t[TAILLE_JEU][TAILLE_JEU], Serpent *s){
+    if(s->tete.x + s->dx >= TAILLE_JEU || s->tete.x + s->dx < 0){
+        exit(1);
+    }else if(s->tete.y + s->dy >= TAILLE_JEU || s->tete.y + s->dy < 0){
+        exit(1);
+    }else{
+        printf("%d %d\n", s->dy, s->dx);
+        s->tete.x += s->dx;
+        s->tete.y += s->dy;
+    }
+}
+
 void pos_s(char t[TAILLE_JEU][TAILLE_JEU], Serpent *s){
-    t[s->tete.x][s->tete.y] = 'H';
+    printf("pos_s: %d %d\n", s->tete.y, s->tete.x);
+    t[s->tete.y][s->tete.x] = 'H';
 }
 
 void remplir_t(char t[TAILLE_JEU][TAILLE_JEU]){
@@ -37,12 +74,15 @@ void remplir_t(char t[TAILLE_JEU][TAILLE_JEU]){
 }
 
 void pos_cible(char t[TAILLE_JEU][TAILLE_JEU], Fruit *f){
-    int x = rand() % 8;
-    int y = rand() % 8;
-    f->x = x;
-    f->y = y;
+    if(f->hit == 1){
+        int x = rand() % 20;
+        int y = rand() % 20;
+        f->x = x;
+        f->y = y;
+    }
 
-    t[x][y] = 'x';
+    t[f->y][f->x] = 'x';
+
 }
 
 void afficher_t(char t[TAILLE_JEU][TAILLE_JEU]){
@@ -59,23 +99,36 @@ void afficher_t(char t[TAILLE_JEU][TAILLE_JEU]){
 int main(int argc, char **argv){
     
     char t[TAILLE_JEU][TAILLE_JEU];
+    char deplacement;
 
     Serpent s = {
         1,
-        1,
-        1,
+        0,
+        0,
         {10,10},
     };
 
     Fruit f;
+    f.hit = 1;
+    pos_cible(t, &f);
+    f.hit = 0;
 
     /* APPEL DES FONCTIONS */
     srand(time(0));
 
-    remplir_t(t);
-    pos_cible(t, &f);
-    pos_s(t, &s);
-    afficher_t(t);
+
+    while(1){
+        remplir_t(t);
+        pos_cible(t, &f);
+
+        pos_s(t, &s);
+        afficher_t(t);
+        scanf("%c", &deplacement);
+        verif_depl(deplacement, &s);
+        depl_s(t, &s);
+
+        
+    }
 
     return 0;
 }
